7cc883091f8db6913b708f199b4581fdbae2b637
Flowers1-Mishtu <87982268+Flowers1-Mishtu@users.noreply.github.com>
2022-10-09 14:13:57 +0100

    Add files via upload

---
+++ lets chat_room.js
@@ -0,0 +1,25 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+const firebaseConfig = {
+      apiKey: "AIzaSyCd4uPtEz6MPclhUND2muceCdEFJQQ1Ww0",
+      authDomain: "lets-chat-73d2c.firebaseapp.com",
+      projectId: "lets-chat-73d2c",
+      storageBucket: "lets-chat-73d2c.appspot.com",
+      messagingSenderId: "497394898521",
+      appId: "1:497394898521:web:610eef2608829e4ab30348",
+      measurementId: "G-LENXPZWG8W"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome "+ user_name+ "!!";
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
